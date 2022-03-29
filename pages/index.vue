f35ade0f0727370ebcfdaa98eead74c990e28bc1
MKCHAN <mkchan@mecaca.com>
2022-03-29 13:44:45 +0800

    update homepage with logo, title and points

--- pages/index.vue
+++ pages/index.vue
@@ -1,8 +1,44 @@
 <template>
-  <div class="flex h-screen bg-slate-600 h-[100vh] align-middle text-white text-center">
-    <div class="m-auto">
-      <h1 class="text-5xl">Welcome to Zero to Marketing!</h1>
-      <p class="mt-5">Turn your ideas into working web applications.</p>
+  <div class="flex h-screen bg-sky-900 h-[100vh] align-middle text-black text-left">
+    <div class="relative px-6 pt-10 pb-8 bg-white shadow-xl ring-1 ring-gray-900/5 sm:max-w-lg sm:mx-auto sm:rounded-lg sm:px-10">
+    <div class="max-w-md m-auto">
+      <img src="https://s3.us-west-2.amazonaws.com/secure.notion-static.com/266b75a4-a040-4936-a1d2-0cc20c8a9943/MECACA-Structured-Data.png?X-Amz-Algorithm=AWS4-HMAC-SHA256&X-Amz-Content-Sha256=UNSIGNED-PAYLOAD&X-Amz-Credential=AKIAT73L2G45EIPT3X45%2F20220329%2Fus-west-2%2Fs3%2Faws4_request&X-Amz-Date=20220329T052029Z&X-Amz-Expires=86400&X-Amz-Signature=8d7af24f57fac49e98277a92fd56996c877fbf25da88166fda1e65fb58a6a5ad&X-Amz-SignedHeaders=host&response-content-disposition=filename%20%3D%22MECACA-Structured-Data.png%22&x-id=GetObject" alt="MECACA">
+      <div class="divide-y divide-gray-300/50">
+        <div class="py-8 text-base leading-7 space-y-6 text-gray-600">
+      <h1 class="text-3xl">Welcome to MECACA</h1>
+      <p class="mt-5">Digital Transformation - Innovation for the New Normal</p>
+      <ul class="space-y-4">
+            <li class="flex items-center">
+              <svg class="w-6 h-6 flex-none fill-sky-100 stroke-sky-500 stroke-2" stroke-linecap="round" stroke-linejoin="round">
+                <circle cx="12" cy="12" r="11" />
+                <path d="m8 13 2.165 2.165a1 1 0 0 0 1.521-.126L16 9" fill="none" />
+              </svg>
+              <p class="ml-4">
+                Tranforming your
+                <font class="text-md font-bold text-gray-900">TRADITIONAL BUSINESS</font> into online business.
+              </p>
+            </li>
+            <li class="flex items-center">
+              <svg class="w-6 h-6 flex-none fill-sky-100 stroke-sky-500 stroke-2" stroke-linecap="round" stroke-linejoin="round">
+                <circle cx="12" cy="12" r="11" />
+                <path d="m8 13 2.165 2.165a1 1 0 0 0 1.521-.126L16 9" fill="none" />
+              </svg>
+              <p class="ml-4">
+                Unleash the unlimited potential by building your business
+                <font class="text-md font-bold text-gray-900">WEB APPLICATION</font>
+              </p>
+            </li>
+            <li class="flex items-center">
+              <svg class="w-6 h-6 flex-none fill-sky-100 stroke-sky-500 stroke-2" stroke-linecap="round" stroke-linejoin="round">
+                <circle cx="12" cy="12" r="11" />
+                <path d="m8 13 2.165 2.165a1 1 0 0 0 1.521-.126L16 9" fill="none" />
+              </svg>
+              <p class="ml-4">Contact us to know more about<br/><font class="text-md font-bold text-gray-900">HOW TO DIGITAL TRANSFORMATION</font></p>
+            </li>
+          </ul>
+    </div>
+    </div>
+    </div>
     </div>
   </div>
 </template>

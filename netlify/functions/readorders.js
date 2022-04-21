d5b4aa9b6f2b21581de774075d8cc034655abb6d
MKChan <mkchan@mecaca.com>
2022-04-21 19:25:44 +0800

    orders 3

--- netlify/functions/readorders.js
+++ netlify/functions/readorders.js
@@ -21,8 +21,8 @@ exports.handler = async function (event, context) {
       };
   });
 
-  return{
-      statusCode = 200,
+  return {
+      statusCode: 200,
       body: JSON.stringify(results),
-  };
+  }
 };
